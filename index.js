f4076f682712087075b7473d6b9f4eadc5d42533
jlonetree <jlonetree@gmail.com>
2020-10-05 18:08:42 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,32 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time){
+  let new_time = time.split(':')
+  let current_time = new_time[0].concat(new_time[1])
+  let result = parseInt(current_time, 10)
+
+  if (result < 1200){
+    return "Good Morning";
+  }
+
+  if ((result >= 1200) && (result <= 1700)){
+    return "Good Afternoon";
+  }
+
+  else if (result > 1700){
+    return "Good Evening";
+  }
+
+  else{
+    return "TEST"
+  }
+
+
+}
+/* Write your implementation of displayMessage() */  
+
+function displayMessage(content) {
+  let text = document.getElementById('greeting')
+  text.innerText = content
+}
